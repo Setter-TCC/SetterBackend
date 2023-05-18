51233276a06dd6c31dbe00cc3e0b8d52c6c344dd
Hugo Sobral <hugosobral10@gmail.com>
2023-05-18 18:50:31 -0300

    [us-01] Init project architecture refactoring

---
+++ src/configs/environment.py
@@ -0,0 +1,24 @@
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+@lru_cache
+def get_env_filename():
+    runtime_env = os.getenv("ENV")
+    return ".env.local"
+
+
+class EnvironmentSettings(BaseSettings):
+    BASE_PATH: str
+    DATABASE_URL: str
+
+    class Config:
+        env_file = get_env_filename()
+        env_file_encoding = "utf-8"
+
+
+@lru_cache
+def get_environment_variables() -> EnvironmentSettings:
+    return EnvironmentSettings()
