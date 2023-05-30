1d381d3e48c8d3b5f5cf46c8a42ae35c5ce6911e
Hugo Sobral <hugosobral10@gmail.com>
2023-05-30 17:42:41 -0300

    [us-01] Remove deprecated files from old project structure

--- src/constants.py
+++
@@ -1,4 +0,0 @@
-from decouple import config
-
-BASE_PATH = config("BASE_PATH", default="/api")
-DATABASE_URL = config("DATABASE_URL", default="postgresql://postgres:password@localhost:5432/setter")
--- src/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-from src.config import DATABASE_URL
-
-Engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
-Base = declarative_base()
