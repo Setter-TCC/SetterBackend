b28b3b6195f9ff1d9d07b0bfb32218b3e839bb62
Hugo Sobral <hugosobral10@gmail.com>
2023-05-30 18:41:03 -0300

    [us-01] Create script to force backend image to waint untill postgres is up

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir -r /app/requirements.txt
 COPY ./src /app/src
-CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
+COPY ./scripts /app/scripts
+CMD ["source", "/app/scripts/start.sh"]
---
+++ scripts/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exporting all environment variables to use in crontab
+env | sed 's/^\(.*\)$/ \1/g' > /root/env
+
+# Função que espera o postgres ficar pronto antes de subir o server
+function_postgres_ready() {
+python << END
+import socket
+import time
+import os
+
+port = int(5432)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('db', port))
+s.close()
+END
+}
+
+echo '======= CHECKING FOR UNINSTALLED PKGs AND INSTALLING'
+pip install -r requirements.txt
+
+until function_postgres_ready; do
+  >&2 echo "======= POSTGRES IS UNAVAILABLE, WAITING"
+  sleep 1
+done
+echo "======= POSTGRES IS UP, CONNECTING"
+
+echo '======= MAKING MIGRATIONS'
+alembic upgrade head
+
+echo '======= RUNNING SERVER'
+uvicorn src.main:app --host 0.0.0.0 --port 8000
