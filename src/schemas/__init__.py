969a6454a0b78ff1b2ca585f9835643b8ebc9c6e
Hugo Sobral <hugosobral10@gmail.com>
2023-06-05 09:10:38 -0300

    [us-03] Add schema for login request

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,6 +1,6 @@
 from src.schemas.administrador import AdministradorSchema
 from src.schemas.atleta import AtletaSchema
-from src.schemas.conta import ContaRequest
+from src.schemas.conta import ContaRequest, LoginSchema
 from src.schemas.integracao import IntegracaoIntegraSchema
 from src.schemas.pessoa import PessoaSchema
 from src.schemas.tecnico import TreinadorSchema
