8bc129b2b32a09eeaf8c8b07aca06a969489f6c1
Hugo Sobral <hugosobral10@gmail.com>
2023-06-07 17:01:03 -0300

    [us-03] Add token validation middleware

--- src/utils/jwt.py
+++ src/utils/jwt.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 from typing import Dict, Union
 
-from jose import jwt
+from fastapi import HTTPException, status
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
 
 from src.configs.environment import get_environment_variables
+from src.repositories import admin_repository
 
 
 def generate_payload(username: str, expire_in: int = 30) -> Dict[str, Union[str, datetime]]:
@@ -24,5 +27,28 @@ def generate_token(payload: Dict[str, Union[str, datetime]]):
 
     return {
         "token": access_token,
-        "exp": payload["exp"]
+        "exp": payload.get("exp")
     }
+
+
+def verify_token(access_token, db: Session) -> dict:
+    env = get_environment_variables()
+    SECRET_KEY = env.SECRET_KEY
+    ALGORITHM = env.ALGORITHM
+
+    try:
+        data = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        admin_on_db = admin_repository.get_admin_by_nome_usuario(db=db, nome_usuario=data.get("sub"))
+        if admin_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid access token"
+            )
+
+        return data
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid access token"
+        )
