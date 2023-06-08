1f6fa92fa9efabe2e3c2a956a69eecd3d6e43d32
Hugo Sobral <hugosobral10@gmail.com>
2023-06-07 23:59:14 -0300

    [us-03] Adjust refresh token router

--- src/routers/token.py
+++ src/routers/token.py
@@ -8,19 +8,19 @@ from src.utils.jwt import generate_payload, generate_token, verify_token
 token_router = APIRouter(prefix="/token")
 
 
-@token_router.post("/refresh", tags=["Token"])
-async def refresh_token(refresh, db: Session = Depends(get_db)):
-    token_data = verify_token(refresh, db=db)
+@token_router.get("/refresh", tags=["Token"])
+async def refresh_token(token, db: Session = Depends(get_db)):
+    token_data = verify_token(token, db=db)
     username = token_data.get("sub")
     token_paylaod = generate_payload(username=username)
-    token = generate_token(token_paylaod)
+    _token = generate_token(token_paylaod)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
             "token": {
-                "token": token.get("token"),
-                "refresh": token.get("refresh"),
-                "expire": token.get("exp")
+                "token": _token.get("token"),
+                "refresh": _token.get("refresh"),
+                "expire": _token.get("exp")
             }
         }
     )
