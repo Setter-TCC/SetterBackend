f064541dc6023c470bf714108146dd020de814fa
Hugo Sobral <hugosobral10@gmail.com>
2023-06-19 20:26:02 -0300

    [us-12] Add admin configuration functions

---
+++ src/routers/admin.py
@@ -0,0 +1,120 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import JSONResponse
+
+from src.configs.database import get_db
+from src.internal.validators import token_validator
+from src.repositories import admin_repository, pessoa_repository
+from src.schemas import AdministradorUpdate, PessoaSchema, AdministradorSchema
+from src.utils.crypt import crypt_context
+
+admin_router = APIRouter(prefix="/admin", dependencies=[Depends(token_validator)])
+
+
+@admin_router.get("", tags=["Admin"])
+async def get_admin_data(admin_id: UUID, db: Session = Depends(get_db), token: dict = Depends(token_validator)):
+    username = token.get("sub")
+    admin = admin_repository.get_admin_by_id(db=db, id_admin=admin_id)
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Admin not found."
+        )
+
+    if admin.nome_usuario != username:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to retrieve data from this admin."
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "msg": "Success fetching admin.",
+            "value": {
+                "id": str(admin.id),
+                "nome": admin.pessoa.nome,
+                "email": admin.pessoa.email,
+                "data_nascimento": admin.pessoa.data_nascimento.strftime("%d/%m/%Y"),
+                "cpf": admin.pessoa.cpf,
+                "rg": admin.pessoa.rg,
+                "telefone": admin.pessoa.telefone,
+                "nome_usuario": admin.nome_usuario
+            }
+        }
+    )
+
+
+@admin_router.put("/update", tags=["Admin"])
+async def update_team(request: AdministradorUpdate, db: Session = Depends(get_db),
+                      token: dict = Depends(token_validator)):
+    if not request.id:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Request body is not well formated."
+        )
+
+    username = token.get("sub")
+    admin_on_db = admin_repository.get_admin_by_id(db=db, id_admin=request.id)
+
+    if not admin_on_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Admin not found."
+        )
+
+    if admin_on_db.nome_usuario != username:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to update this admin."
+        )
+
+    if request.senha and request.nova_senha:
+        if not crypt_context.verify(request.senha, admin_on_db.senha):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid password."
+            )
+
+    pessoa = PessoaSchema(
+        id=request.id,
+        nome=request.nome,
+        email=request.email,
+        cpf=request.cpf,
+        rg=request.rg,
+        data_nascimento=request.data_nascimento,
+        telefone=request.telefone
+    )
+
+    admin = AdministradorSchema(
+        id=request.id,
+        nome_usuario=request.nome_usuario,
+        senha=request.nova_senha
+    )
+
+    pessoa_ok = pessoa_repository.update_pessoa(db=db, pessoa=pessoa)
+    if not pessoa_ok:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_424_FAILED_DEPENDENCY,
+            detail="Could not update personal data for admin."
+        )
+
+    if request.senha and request.nova_senha:
+        admin_ok = admin_repository.update_admin(db=db, admin=admin)
+        if not admin_ok:
+            db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_424_FAILED_DEPENDENCY,
+                detail="Could not update account data for admin."
+            )
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "msg": "Updated admin succesfully."
+        }
+    )
--- src/schemas/administrador.py
+++ src/schemas/administrador.py
@@ -1,6 +1,28 @@
+from typing import Optional
+from uuid import UUID
+
 from src.schemas.pessoa import PessoaSchema
+
+from pydantic import BaseModel, EmailStr
 
 
 class AdministradorSchema(PessoaSchema):
     nome_usuario: str
     senha: str
+
+
+class AdministradorUpdate(BaseModel):
+    id: Optional[UUID]
+    nome: str
+    email: EmailStr
+    cpf: Optional[str] = None
+    rg: Optional[str] = None
+    data_nascimento: Optional[datetime] = None
+    telefone: Optional[str] = None
+    nome_usuario: str
+    senha: Optional[str] = None
+    nova_senha: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
