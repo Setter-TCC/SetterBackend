5fb90b4a78b5bec7d7f469e0f2d59488743a1049
Hugo Sobral <hugosobral10@gmail.com>
2023-06-22 20:17:59 -0300

    [us-21] Add transaction internal functions

---
+++ src/internal/transaction.py
@@ -0,0 +1,31 @@
+from src.utils.enums import TipoTransacao
+
+
+def get_transaction_origin(team_name: str, transaction_name: str, person_name: str,
+                           tipo_transacao: TipoTransacao) -> str:
+    if tipo_transacao == TipoTransacao.despesa or tipo_transacao == TipoTransacao.salario_tecnico:
+        return team_name
+
+    elif tipo_transacao == TipoTransacao.ganho:
+        return transaction_name
+
+    elif tipo_transacao == TipoTransacao.mensalidade:
+        return person_name
+
+    else:
+        return ""
+
+
+def get_transaction_destiny(team_name: str, transaction_name: str, person_name: str,
+                            tipo_transacao: TipoTransacao) -> str:
+    if tipo_transacao == TipoTransacao.ganho or tipo_transacao == TipoTransacao.mensalidade:
+        return team_name
+
+    elif tipo_transacao == TipoTransacao.salario_tecnico:
+        return person_name
+
+    elif tipo_transacao == TipoTransacao.despesa:
+        return transaction_name
+
+    else:
+        return ""
