3cf5b0699023317c76e0b0b07793d9e05bec9e2b
Hugo Sobral <hugosobral10@gmail.com>
2023-07-17 18:05:36 -0300

    Improve return messages

--- src/internal/validators.py
+++ src/internal/validators.py
@@ -27,13 +27,27 @@ async def validate_user_authorization(db: Session, time_id: UUID, token: dict):
     if not integracao_on_db:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User is not allowed to modify this team."
+            detail="User is not allowed to modify this team because it was not found."
+        )
+
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    print(f"IntegracaoIntegra")
+    print(f"ativo:       {integracao_on_db.ativo}")
+    print(f"tipo pessoa: {integracao_on_db.tipo_pessoa.name}")
+    print(f"time_id:     {str(integracao_on_db.time_id)}")
+    print(f"pessoa_id:   {str(integracao_on_db.pessoa_id)}")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+    if not integracao_on_db.ativo:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is not allowed to modify this team becausa it is not active. aaaaaaaaaaaa"
         )
 
     if not integracao_on_db.ativo or integracao_on_db.tipo_pessoa != TipoPessoa.administrador:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User is not allowed to modify this team."
+            detail=f"User is not allowed to modify this team becausa it is not an admin. He is {integracao_on_db.tipo_pessoa.name}"
         )
 
 
